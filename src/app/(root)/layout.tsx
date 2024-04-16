d893c7af723ccc0a1ab50dd10f4ea9d98c8c7ca5
Sanket Shinde <sanketshinde9401@gmail.com>
2024-04-16 23:53:46 +0530

    add sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,7 @@
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div>
-      Navbar
-      {children}
-      Footer
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default RootLayout;
