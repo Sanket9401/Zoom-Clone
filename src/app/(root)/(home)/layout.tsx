0c74b6822cd44d4a97649904e558d31480f84d14
Sanket Shinde <sanketshinde9401@gmail.com>
2024-04-18 23:52:33 +0530

    add navbar

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -5,11 +5,11 @@ import React, { ReactNode } from "react";
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="relative">
+    <main className="relative overflow-hidden">
       <Navbar />
       <div className="flex">
         <Sidebar />
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+        <section className="flex size-full flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
       </div>
