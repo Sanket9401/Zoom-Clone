e5084d1d499615d4b44d88d3c6a3d1c49ea1c637
Sanket Shinde <sanketshinde9401@gmail.com>
2024-04-19 23:17:05 +0530

    integrate clerk

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import { Link } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import MobileNav from "./MobileNav";
+import Link from "next/link";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
@@ -14,11 +15,12 @@ const Navbar = () => {
           alt="Zoom logo"
           className="max-sm:size-10"
         />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          Zoom
-        </p>
+        <p className="text-[26px] font-extrabold text-white">Zoom</p>
       </Link>
       <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <MobileNav />
       </div>
     </nav>
