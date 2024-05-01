a98a805e8f3e70566cf566fbab4b7ebd644d758f
Sanket Shinde <sanketshinde9401@gmail.com>
2024-05-01 22:00:45 +0530

    feat: create meeting

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useUser } from "@clerk/nextjs";
 import {
   StreamCall,
@@ -6,6 +7,8 @@ import {
   User,
 } from "@stream-io/video-react-sdk";
 import { ReactNode, useEffect, useState } from "react";
+import { tokenProvider } from "../actions/stream.actions";
+import Loader from "@/components/Loader";
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
@@ -27,8 +30,15 @@ export default function StreamVideoProvider({
       user: {
         id: user?.id,
         name: user?.username || user?.id,
+        image: user?.imageUrl,
       },
+      tokenProvider,
     });
+
+    setVideoClient(client);
   }, [user, isLoaded]);
-  return <StreamVideo client={videoClient}></StreamVideo>;
+
+  if (!videoClient) return <Loader />;
+
+  return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 }
