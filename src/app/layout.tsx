61265035060ca09f2f80f019fa0b21157bfc5d7d
Sanket Shinde <sanketshinde9401@gmail.com>
2024-05-18 22:19:07 +0530

    feat: complete schedule meeting section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import { Toaster } from "@/components/ui/toaster";
 
+import "react-datepicker/dist/react-datepicker.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
